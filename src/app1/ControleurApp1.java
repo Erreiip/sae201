30d1d5bd427633d1c95a1eecd8f71223ffe19a7e
KiwiTerra <kiwiterra@gmail.com>
2022-06-10 13:59:39 +0200

    Mise à jour du Main

--- src/app1/ControleurApp1.java
+++ src/app1/ControleurApp1.java
@@ -18,7 +18,23 @@ public class ControleurApp1
         
     }
 
-    public static void matriceAdjacence(){
+    public static void main(String[] args) {
+        if(args.length > 0 && args[0].equals("debug")){
+            lancerModeDebug();
+            return;
+        }
+
+        int choixMatrice;
+        do {
+            System.out.println("Veuillez chosir une matrice :\n" +
+                    "[1] - Liste d'adjacence \n" +
+                    "[2] - Matrice de cout   \n" +
+                    "[3] - Matrice de cout optimisé");
+
+            System.out.print("Veuillez choisir entre 1 et 3 : ");
+            choixMatrice = Clavier.lire_int();
+        } while (choixMatrice < 1 || choixMatrice > 3);
+
 
         while (Cuve.getNbCuves() < 26) {
 
@@ -44,63 +60,12 @@ public class ControleurApp1
 
             System.out.println("La cuve " + cuveEnCreation.getIdentifiant() + " a été créée avec succès.");
         }
+
     }
 
-    public static void main(String[] args) {
-
-
-        if (args.length == 0){
-            System.out.println("Vide");
-        }
-        else {
-
-            applicationDebug(args);
-
-        }
-
-        int choixMatrice;
-
-        System.out.println("Veuillez chosir une matrice :\n" +
-                "[1] - Liste d'adjacence \n" +
-                "[2] - Matrice de cout   \n" +
-                "[3] - Matrice de cout optimisé");
-
-        System.out.print("Veuillez choisir entre 1 et 3 : ");
-
-        while (true) {
-
-            choixMatrice = Clavier.lire_int();
-
-            if (choixMatrice < 1 || choixMatrice > 3){
-
-
-                System.out.print("Entrez invalide, veuillez choisir entre 1 et 3 : ");
-
-            }
-            else {
-                break;
-
-            }
-
-            switch (choixMatrice) {
-                case 1:
-                    matriceAdjacence();
-                    break;
-
-                case 2:
-                    System.out.println("Pas implémenté");
-
-                case 3:
-                    System.out.println("Pas implémenté");
-
-                default:
-                    break;
-            }
-
-        }
-
-
-
+    private static void lancerModeDebug() {
+        ControleurApp1.debug = true;
+        System.out.println("Mode debug activé");
     }
 
 }
