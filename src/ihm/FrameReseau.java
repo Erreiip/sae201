f21e7290dcca60f335a58814117ca11f37628d0a
Erreip <lolpierre154@gmail.com>
2022-06-10 11:32:34 +0200

    Ajustements mineurs sur les Frames

--- src/ihm/FrameReseau.java
+++ src/ihm/FrameReseau.java
@@ -10,24 +10,24 @@ import src.Reseau;
 public class FrameReseau extends JFrame 
 {
 
-    private Reseau reseau;
+    private Controleur ctrl;
 
     private PanelPaint panelPrincipal;
 
-    public FrameReseau ( Reseau reseau )
+    public FrameReseau ( Controleur ctrl )
     {
         Dimension d = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
         int height  = d.height;
         int width   = d.width;
 
-        this.reseau = reseau;
+        this.ctrl = ctrl;
         
         /*-------------------------------------------------*/
 
         this.setSize    ( height/2, width/2 );
         this.setLocation( height/2 - this.HEIGHT, width/2 - this.WIDTH );
 
-        this.panelPrincipal = new PanelPaint( this );
+        this.panelPrincipal = new PanelPaint( this.ctrl );
     }
 
 
