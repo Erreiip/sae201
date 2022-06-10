8aeba37ea80acef125d17c98e3082f6ac65d29f9
KiwiTerra <kiwiterra@gmail.com>
2022-06-10 10:37:03 +0200

    Retrait de l'attribut ordinal

--- src/Position.java
+++ src/Position.java
@@ -2,15 +2,8 @@ package src;
 
 public enum Position
 {
-    HAUT   (0),
-    BAS    (1),
-    GAUCHE (2),
-    DROITE (3);
-
-    int ordinal;
-
-    Position ( int oridinal ) { this.ordinal = ordinal;}
-
-    public int getOrdial() { return this.ordinal; }
-
+    HAUT   ,
+    BAS    ,
+    GAUCHE ,
+    DROITE ;
 }
