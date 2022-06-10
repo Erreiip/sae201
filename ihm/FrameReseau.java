30b425a3a31d8add83621d393b1c8fa1bcce3d4b
Erreip <lolpierre154@gmail.com>
2022-06-10 10:34:27 +0200

    ajout de l'ihm

---
+++ ihm/FrameReseau.java
@@ -0,0 +1,17 @@
+package ihm;
+import javax.swing.*;
+
+import Reseau;
+
+public class FrameReseau extends JFrame 
+{
+
+    public FrameReseau ( Reseau reseau )
+    {
+        Dimension d = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
+
+        
+    }
+
+
+}
