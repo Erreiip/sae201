45835476ce09072e6a76d2a1e936bc357c25ab3d
Erreip <lolpierre154@gmail.com>
2022-06-10 10:10:58 +0200

    finalisatio de la classe Position

--- Position.java
+++ Position.java
@@ -1,4 +1,14 @@
 public enum Position 
 {
-    
+    HAUT   (0),
+    BAS    (1),
+    GAUCHE (2),
+    DROITE (3);
+
+    int ordinal;
+
+    Position ( int oridinal ) { this.ordinal = ordinal;}
+
+    public int getOrdial() { return this.ordinal; }
+
 }
