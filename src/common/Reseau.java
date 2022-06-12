7f73470c7174cece3a23c16606bc90f4aa608034
DUC Dinh <ducthai2001177@gmail.com>
2022-06-12 02:04:13 +0200

    Add methods : toStringListAdjacence(), toStringMatriceCout() ,getMatriceCout()

--- src/common/Reseau.java
+++ src/common/Reseau.java
@@ -8,23 +8,23 @@ import java.util.ArrayList;
  */
 public class Reseau 
 {
-    private final ArrayList<Tuyau> tuyaux;
-    private final ArrayList<Cuve>  cuves;
+    private final ArrayList<Tuyau> listTuyaux;
+    private final ArrayList<Cuve>  listCuves;
 
     // Constructeur
     public Reseau()
     {
-        this.tuyaux = new ArrayList<>();
-        this.cuves  = new ArrayList<>();
+        this.listTuyaux = new ArrayList<>();
+        this.listCuves  = new ArrayList<>();
     }
 
     // Accesseurs
-    public ArrayList<Cuve>  getCuves()  { return cuves; }
-    public ArrayList<Tuyau> getTuyaux() { return tuyaux; }
+    public ArrayList<Cuve>  getCuves()  { return listCuves; }
+    public ArrayList<Tuyau> getTuyaux() { return listTuyaux; }
 
     public Cuve getCuve(char identifiant)
     {
-        for (Cuve c : this.cuves)
+        for (Cuve c : this.listCuves)
         {
             if ( c.getIdentifiant() == identifiant )
             {
@@ -41,7 +41,7 @@ public class Reseau
 
     public Tuyau getTuyau(Cuve cuve1, Cuve cuve2)
     {
-        for(Tuyau t : this.tuyaux)
+        for(Tuyau t : this.listTuyaux)
         {
             if (t.estRelie(cuve1) && t.estRelie(cuve2))
             {
@@ -81,43 +81,76 @@ public class Reseau
     }
 
     /**
-     * Retourne vrai si les deux cuves sont reliées par un tuyau dans ce réseau.
+     * Retourne vrai si les deux listCuves sont reliées par un tuyau dans ce réseau.
      */
     public boolean sontRelies(Cuve cuve1, Cuve cuve2)
     {
         return getTuyau(cuve1, cuve2) != null;
     }
-
-    public String getMatriceCout()
+    public int[][] getMatriceCout()
     {
-		String sRet = "";
-        String [][] matrice;
+        int [][] matrice;
         int nbCuves = this.getCuves().size();
-        matrice = new String[nbCuves][nbCuves];
+        matrice = new int[nbCuves][nbCuves];
         for (Tuyau tuyau : this.getTuyaux())
         {
             int lig,col;
             lig = tuyau.getCuve1().getIdentifiant() - 'A';
             col = tuyau.getCuve2().getIdentifiant() - 'A';
-            matrice[lig][col] = tuyau.getSection() + "" ;
-            matrice[col][lig] = tuyau.getSection() + "";
+            matrice[lig][col] = tuyau.getSection() ;
+            matrice[col][lig] = tuyau.getSection() ;
         }
+        return matrice;
+    }
+    public String toStringMatriceCout()
+    {
+		String sRet = "";
+        int [][] matrice;
+        matrice = this.getMatriceCout();
+        int nbCuves = this.getMatriceCout().length;
+
         for (int lig =0;lig < nbCuves; lig ++)
         {
             for (int col = 0; col < nbCuves; col ++)
             {
-                sRet += matrice[lig][col] != null ? String.format("%3s",matrice[lig][col]):"  0";
+                sRet += String.format("%3d",matrice[lig][col]);
+                /*default value of integer array is 0*/
             }
-            
             sRet += "\n";
         }
         return sRet;           
     }
+    public String toStringListAdjac()
+    {
+        String sRet = "List d'adjacence:\n";
+        for (Tuyau t :this.listTuyaux)
+            sRet += t.getCuve1().getIdentifiant() + t.getCuve2().getIdentifiant() + "\n";
+        
+        return sRet;
+    }
+    public String toStringMatriceCoutOptimise()
+    {
+        String sRet = "";
+        int[][] matrice = this.getMatriceCout();
+        int etage = 1;
+        
+        for (int lig =0;lig < matrice.length; lig ++)
+        {
+            for (int col = 0; col < etage; col ++)
+            {
+                sRet += String.format("%3d",matrice[lig][col]);
+                /*default value of integer array is 0*/
+                etage ++;
+            }
+            sRet += "\n";
+        }
+        return sRet; 
+    }
     @Override
     public String toString() {
         return "Reseau{" +
-                "tuyaux=" + tuyaux +
-                ", cuves=" + cuves +
+                "listTuyaux=" + listTuyaux +
+                ", listCuves=" + listCuves +
                 '}';
     }
 }
