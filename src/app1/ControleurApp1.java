5d561c6330816608a6eca9b695006fb13c60399c
KiwiTerra <kiwiterra@gmail.com>
2022-06-14 09:26:01 +0200

    Modification du Main

--- src/app1/ControleurApp1.java
+++ src/app1/ControleurApp1.java
@@ -19,7 +19,7 @@ public class ControleurApp1
 {
     private static final boolean debug = false;
 
-    private Reseau reseau;
+    private Reseau    reseau;
     private FrameApp1 ihm;
 
     public ControleurApp1()
@@ -27,8 +27,15 @@ public class ControleurApp1
         this.reseau = new Reseau();
     }
 
-    public List<Cuve> getCuves()   { return this.reseau.getCuves(); }
-    public List<Tuyau> getTuyaux() { return this.reseau.getTuyaux(); }
+    public List<Cuve> getCuves()
+    {
+        return this.reseau.getCuves();
+    }
+
+    public List<Tuyau> getTuyaux()
+    {
+        return this.reseau.getTuyaux();
+    }
 
     public boolean creerTuyau(int section, Cuve cuve1, Cuve cuve2)
     {
@@ -38,32 +45,32 @@ public class ControleurApp1
     public boolean creerTuyau(int section, char idCuve1, char idCuve2)
     {
         Tuyau tuyau = reseau.creerTuyau(section, idCuve1, idCuve2);
-        if( tuyau == null ) return false;
+        if (tuyau == null) return false;
 
-        if(this.ihm != null) this.ihm.majListeTuyaux();
+        if (this.ihm != null) this.ihm.majListeTuyaux();
 
         return true;
     }
 
-    public boolean creerCuve(int capacite )
+    public boolean creerCuve(int capacite)
     {
         Cuve cuve = reseau.creerCuve(capacite);
-        if ( cuve == null ) return false;
+        if (cuve == null) return false;
 
-        if(ihm != null) this.ihm.majListeCuves();
+        if (ihm != null) this.ihm.majListeCuves();
 
         return true;
     }
 
     public boolean supprimerCuve()
     {
-        if(this.ihm == null) return false;
+        if (this.ihm == null) return false;
 
         int ligne = this.ihm.getCuveActive();
 
-        if(ligne == -1) return false;
+        if (ligne == -1) return false;
 
-        if(!this.reseau.supprimerCuve(ligne)) return false;
+        if (!this.reseau.supprimerCuve(ligne)) return false;
 
         this.ihm.majListeCuves();
         this.ihm.majListeTuyaux();
@@ -72,99 +79,56 @@ public class ControleurApp1
 
     public void setReseau(String absolutePath)
     {
-        try {
+        try
+        {
             FichierReseau fichierReseau = FichierReseau.fromString(Files.readString(Path.of(absolutePath)));
             this.reseau = fichierReseau.getReseau();
-        } catch (IOException e) {
+        } catch (IOException e)
+        {
             e.printStackTrace();
         }
     }
 
-    public void sauvegarderReseau(ReseauFormatType reseauFormatType, String path) {
+    public void sauvegarderReseau(ReseauFormatType reseauFormatType, String path)
+    {
         try
         {
-            Files.writeString(Path.of(path), reseauFormatType.getFormat().toString(this.reseau));
-        } catch (Exception e){ e.printStackTrace(); }
+            FichierReseau fichierReseau = new FichierReseau(reseauFormatType, this.reseau);
+            Files.writeString(Path.of(path), fichierReseau.toString());
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 
-    public FrameApp1 getIhm() {
+    public FrameApp1 getIhm()
+    {
         return ihm;
     }
 
-    public void setIhm(FrameApp1 ihm) {
+    public void setIhm(FrameApp1 ihm)
+    {
         this.ihm = ihm;
     }
 
-    public String toStringReseau(){
+    public String toStringReseau()
+    {
         return reseau.toString();
     }
-
-    public void sortieFichierTexteMatriceAdjacente()
-    {
-        String sRet = ReseauFormatType.LISTE_ADJA.getFormat().toString(this.reseau);
-        try
-		{
-			PrintWriter pw = new PrintWriter( new FileOutputStream("sortie.txt") );
-
-            pw.println (sRet);
-
-			pw.close();
-		}
-		catch (Exception e){ e.printStackTrace(); }
-        /*Test sur le console*/
-
-        System.out.println(sRet);
-    }
-
-    public void sortieFichierTexteMatriceCout ()
-    {
-        String sRet = ReseauFormatType.COUTS.getFormat().toString(this.reseau);
-        try
-		{
-			PrintWriter pw = new PrintWriter( new FileOutputStream("sortie.txt") );
-
-            pw.println (sRet);
-
-			pw.close();
-		}
-		catch (Exception e){ e.printStackTrace(); }
-        /*Test sur le console*/
-
-        System.out.println(sRet);
-    }
-
-    public void sortieFichierTexteMatriceCoutOpti()
-    {
-        String sRet = ReseauFormatType.COUTS_OPTI.getFormat().toString(this.reseau);
-        try
-		{
-			PrintWriter pw = new PrintWriter( new FileOutputStream("sortie.txt") );
-
-            pw.println (sRet);
-
-			pw.close();
-		}
-		catch (Exception e){ e.printStackTrace(); }
-        /*Test sur le console*/
-
-        System.out.println(sRet);
-    }
-
-
 
     public static void main(String[] args)
     {
         ControleurApp1 controleur = new ControleurApp1();
-        if(args.length > 0 && args[0].equalsIgnoreCase("gui")) {
+        if (args.length > 0 && args[0].equalsIgnoreCase("gui"))
+        {
             FrameApp1 ihm = new FrameApp1(controleur);
             controleur.setIhm(ihm);
             return;
         }
 
-        int            nbCuves;
-        int            capaciteMaximal;
-        double         capaciteInitiale;
-        boolean        valideCuve;
+        int     nbCuves;
+        int     capaciteMaximal;
+        boolean valideCuve;
 
         /*----------------------------*/
         /**Demander le nombre de cuves*/
@@ -179,115 +143,96 @@ public class ControleurApp1
         /**Creations des cuves */
         /*---------------------*/
         char idCuveEnCreation = 'A';
-        for (int cpt=0; cpt < nbCuves; cpt ++)
+        for (int cpt = 0; cpt < nbCuves; cpt++)
         {
-            /**Renseigner capacite de cuve en creation */
-            do {
-            System.out.print("Entrez la capacité maximale de la cuve "+ idCuveEnCreation + " (entre 200 et 2000) :");
-            capaciteMaximal = Clavier.lire_int();
 
-            valideCuve = false;
+            /* Renseigner capacite de cuve en creation */
+            do
+            {
+                System.out.print("Entrez la capacité maximale de la cuve " + idCuveEnCreation + " (entre 200 et 2000) :");
+                capaciteMaximal = Clavier.lire_int();
 
-            try {
-                valideCuve = controleur.creerCuve(capaciteMaximal);
-            } catch (Exception e) {
-                //TODO: handle exception
-            }
+                valideCuve = false;
 
+                try
+                {
+                    valideCuve = controleur.creerCuve(capaciteMaximal);
+                } catch (Exception e)
+                {
+                    //TODO: handle exception
+                    System.err.println("Erreur: " + e.getMessage());
+                }
 
-
-            if (!valideCuve) { System.out.println("Invalide"); continue ;}
             } while (!valideCuve);
 
             System.out.println("La cuve " + idCuveEnCreation + " a été créée avec succès.");
-            idCuveEnCreation ++;
-        }
-
-
-        if (debug){
-
-            System.out.println(controleur.toStringReseau());
-
+            idCuveEnCreation++;
         }
 
         /*Creation des tuyaux*/
-        char  resCreerTuyau;
-        int   section;
-        char  idCuve1, idCuve2;
+        char    resCreerTuyau;
+        int     section;
+        char    idCuve1, idCuve2;
         boolean valideTuyau;
 
-        System.out.println ("Voulez-vous creer un tuyau (O/N) ?");
 
         do
         {
-
+            System.out.println("Voulez-vous créer un tuyau ? (O/N)");
             resCreerTuyau = Clavier.lire_char();
-            if (resCreerTuyau == 'O')
+
+            valideTuyau = false;
+            do
             {
-                do {
-                    System.out.print ("\nSection ? ");
-                    section = Clavier.lire_int();
+                System.out.println("Entrez la section du tuyau :");
+                section = Clavier.lire_int();
 
-                    System.out.print ("\nidCuve1 ? ");
-                    idCuve1 = Clavier.lire_char();
+                System.out.println("Entrez l'identifiant de la première cuve :");
+                idCuve1 = Clavier.lire_char();
 
-                    System.out.print ("\nidCuve2 ? ");
-                    idCuve2 = Clavier.lire_char();
+                System.out.println("Entrez l'identifiant de la deuxième cuve :");
+                idCuve2 = Clavier.lire_char();
 
-                    valideTuyau = false;
+                try
+                {
+                    valideTuyau = controleur.creerTuyau(section, idCuve1, idCuve2);
+                } catch (Exception e)
+                {
+                    System.err.println("Erreur: " + e.getMessage());
+                }
 
-                    try {
-                        valideTuyau = controleur.creerTuyau(section, idCuve1, idCuve2);
-                    } catch (Exception e) {
-                        //TODO: handle exception
-                    }
-
-                    if (!valideTuyau) System.out.println ("Invalide");
-
-                } while (!valideTuyau);
-            }
-            else break;
-
-            System.out.println ("Voulez-vous continuer de creer un tuyau (O/N) ?");
+            } while (!valideTuyau);
 
         } while (resCreerTuyau == 'O');
 
         /*Sélection de la Matrice voulu*/
-
         int selectionFormat;
 
         do
         {
-            System.out.println("Veuillez choisir le format de la structure de sortie :\n"+
-            "[1]-Liste d'adjacence             \n"+
-            "[2]-Matrice de cout               \n"+
-            "[3]-Matrice de cout optimisée     \n"+
-            "[Autres]-Quitter l'application    \n"+
-            "Veuillez chosir entre 1 et 3 : ");
+
+            String question = "Veuillez choisir le format de la structure de sortie:\n";
+            for (ReseauFormatType value : ReseauFormatType.values())
+            {
+                question += "[" + (value.ordinal() + 1) + "] " + value.getNom() + "\n";
+            }
+            question += "Veuillez choisir entre 1 et " + (ReseauFormatType.values().length + 1) + " :";
+
+            System.out.print(question);
             selectionFormat = Clavier.lire_int();
 
-            switch (selectionFormat) {
+            switch (selectionFormat)
+            {
                 case 1:
-
-                    controleur.sortieFichierTexteMatriceAdjacente();
-                    break;
-
                 case 2:
-
-                    controleur.sortieFichierTexteMatriceCout();
-                    break;
-
                 case 3:
-
-                    controleur.sortieFichierTexteMatriceCoutOpti();
+                    ReseauFormatType reseauFormatType = ReseauFormatType.values()[selectionFormat - 1];
+                    controleur.sauvegarderReseau(reseauFormatType, "reseau.data");
                     break;
 
                 default:
                     break;
             }
-        } while (selectionFormat >=1 && selectionFormat <= 3); 
-        /**selectionFormat doit etre 1 ou 2 ou 3 pour continuer */
-
-
+        } while (selectionFormat >= 1 && selectionFormat <= ReseauFormatType.values().length + 1);
     }
 }
