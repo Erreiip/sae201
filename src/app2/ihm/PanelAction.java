014a0f0b95640c70e7d50ff9ff3821650565f6b5
Erreip <lolpierre154@gmail.com>
2022-06-15 09:58:05 +0200

    Ajout du bouton supprimer contenu

--- src/app2/ihm/PanelAction.java
+++ src/app2/ihm/PanelAction.java
@@ -19,6 +19,7 @@ public class PanelAction extends JPanel implements ActionListener
     private final JTextField txtCuve;
     private final JTextField txtContenu;
     private final JButton    btnAction;
+    private final JButton    btnSupprimer;
     private final JButton    btnInitAll;
 
     private final JButton btnIte;
@@ -46,15 +47,16 @@ public class PanelAction extends JPanel implements ActionListener
 
         JPanel panelContenu = new JPanel(new BorderLayout(3, 3));
         JPanel panelText    = new JPanel(new GridLayout(2, 2));
-        JPanel panelBtn     = new JPanel(new GridLayout(2, 1));
+        JPanel panelBtn     = new JPanel(new GridLayout(3, 1));
 
         JLabel lblCuve    = new JLabel("Identifiant Cuve : ", JLabel.RIGHT);
         JLabel lblContenu = new JLabel("Contenu : ", JLabel.RIGHT);
 
-        this.txtCuve = new JTextField();
-        this.txtContenu = new JTextField();
-        this.btnAction = new JButton("Valider");
-        this.btnInitAll = new JButton("Tout ajouter du contenu");
+        this.txtCuve      = new JTextField();
+        this.txtContenu   = new JTextField();
+        this.btnAction    = new JButton("Ajouter contenu");
+        this.btnSupprimer = new JButton("Supprimer contenu");
+        this.btnInitAll   = new JButton("Tout ajouter du contenu");
 
 
         JPanel panelIteration = new JPanel(new GridLayout(2, 1));
@@ -72,27 +74,29 @@ public class PanelAction extends JPanel implements ActionListener
         panelIteration.add(this.btnIte);
         panelIteration.add(this.btnContinue);
 
-        panelText.add(lblCuve);
-        panelText.add(this.txtCuve);
-        panelText.add(lblContenu);
-        panelText.add(this.txtContenu);
+        panelText.add( lblCuve );
+        panelText.add( this.txtCuve );
+        panelText.add( lblContenu );
+        panelText.add( this.txtContenu );
 
+        panelBtn.add(this.btnSupprimer);
         panelBtn.add(this.btnAction);
         panelBtn.add(this.btnInitAll);
 
-        panelContenu.add(panelText, BorderLayout.NORTH);
-        panelContenu.add(panelBtn, BorderLayout.CENTER);
+        panelContenu.add(panelText   , BorderLayout.NORTH);
+        panelContenu.add(panelBtn    , BorderLayout.CENTER);
         panelContenu.add(this.lblInfo, BorderLayout.SOUTH);
 
 
-        this.add(panelContenu, BorderLayout.NORTH);
-        this.add(this.lblIteration, BorderLayout.CENTER);
-        this.add(panelIteration, BorderLayout.SOUTH);
+        this        .add(panelContenu     , BorderLayout.NORTH);
+        this        .add(this.lblIteration, BorderLayout.CENTER);
+        this        .add(panelIteration   , BorderLayout.SOUTH);
 
-        this.btnAction.addActionListener(this);
-        this.btnInitAll.addActionListener(this);
-        this.btnContinue.addActionListener(this);
-        this.btnIte.addActionListener(this);
+        this.btnAction   .addActionListener(this);
+        this.btnInitAll  .addActionListener(this);
+        this.btnSupprimer.addActionListener(this);
+        this.btnContinue .addActionListener(this);
+        this.btnIte      .addActionListener(this);
 
 
     }
@@ -104,7 +108,7 @@ public class PanelAction extends JPanel implements ActionListener
             try
             {
                 this.ctrl.getMetier().getCuve(this.txtCuve.getText().charAt(0)).ajouterContenu(Integer.parseInt(this.txtContenu.getText()));
-                this.lblInfo.setText("Contenu changé");
+                this.lblInfo.setText( this.txtContenu.getText() + " ajouté à " + this.txtCuve.getText() );
                 this.ctrl.redessiner();
             } catch (Exception ex)
             {
@@ -114,6 +118,22 @@ public class PanelAction extends JPanel implements ActionListener
             this.txtContenu.setText("");
             this.txtCuve.setText("");
 
+        }
+
+        if ( e.getSource() == this.btnSupprimer )
+        {
+            try
+            {
+                this.ctrl.getMetier().getCuve(this.txtCuve.getText().charAt(0)).retirerContenu(Integer.parseInt(this.txtContenu.getText()));
+                this.lblInfo.setText( this.txtContenu.getText() + " retiré à " + this.txtCuve.getText() );
+                this.ctrl.redessiner();
+            } catch (Exception ex)
+            {
+                this.lblInfo.setText("Erreur");
+            }
+
+            this.txtContenu.setText("");
+            this.txtCuve.setText("");
         }
 
         if (e.getSource() == this.btnInitAll)
